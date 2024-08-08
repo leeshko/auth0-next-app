8a61f4f697276b6eb4aa98b70de3ad78c8a567fc
D.Lishko <dmitrilishko@gmail.com>
2024-08-08 14:35:16 +0200

    remove DB connection

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 App demonstrates login page
 
 link for the app, uploaded to Vercel: https://auth0-next-kwqm6jzuc-dimas-projects-affd4092.vercel.app/
+
+DB is not connected
+
+Mongoose used.
+Mongoose does not currently support Next.js Edge Runtime. While you can import Mongoose in Edge Runtime, you'll get Mongoose's browser library. There is no way for Mongoose to connect to MongoDB in Edge Runtime, because Edge Runtime currently doesn't support Node.js net API, which is what the MongoDB Node Driver uses to connect to MongoDB.
--- lib/db.ts
+++ lib/db.ts
@@ -1,41 +1,41 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI as string;
+// const MONGO_URI = process.env.MONGO_URI as string;
 
-if (!MONGO_URI) {
-  throw new Error(
-    "Please define the MONGO_URI environment variable inside .env.local"
-  );
-}
+// if (!MONGO_URI) {
+//   throw new Error(
+//     "Please define the MONGO_URI environment variable inside .env.local"
+//   );
+// }
 
-// Global is used here to maintain a cached connection across hot reloads in development
-declare global {
-  var mongoose: any;
-}
+// // Global is used here to maintain a cached connection across hot reloads in development
+// declare global {
+//   var mongoose: any;
+// }
 
-let cached = global.mongoose;
+// let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
 
-const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+// const connectDB = async () => {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
 
-    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  console.log("=====> Connected!!!");
-  return cached.conn;
-};
+//     cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+//       return mongoose;
+//     });
+//   }
+//   cached.conn = await cached.promise;
+//   console.log("=====> Connected!!!");
+//   return cached.conn;
+// };
 
-export default connectDB;
+// export default connectDB;
